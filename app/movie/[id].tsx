2b43e7a25563f4f984eae236462d6576bea3acaf
nazma98 <nazmasarker1311@gmail.com>
2025-06-12 13:31:51 +0600

    Add routes for onboarding and movie details as experiment

---
+++ app/movie/[id].tsx
@@ -0,0 +1,15 @@
+import { useLocalSearchParams } from 'expo-router';
+import { StyleSheet, Text, View } from 'react-native';
+
+const Details = () => {
+  const { id } = useLocalSearchParams();
+  return (
+    <View>
+      <Text>Movie Details: {id}</Text>
+    </View>
+  );
+};
+
+export default Details;
+
+const styles = StyleSheet.create({});
