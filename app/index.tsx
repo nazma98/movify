2db316eb5f71ee275c98440eaa8d6826e99c4822
nazma98 <nazmasarker1311@gmail.com>
2025-06-12 13:15:12 +0600

    Add tailwindcss config file and metro config file

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,9 @@
-import { Text, View } from "react-native";
+import { Text, View } from 'react-native';
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className='flex-1 justify-center items-center'>
+      <Text className='text-5xl text-blue-500 font-bold'>Welcome!</Text>
     </View>
   );
 }
